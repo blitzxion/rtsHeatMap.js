9930a596b314044cda36c909194371f7f6f2924c
Richard Shaw <richard.shaw@theactigraph.com>
2013-08-24 20:54:45 -0500

    Removed old tempToRGB method.
    Colors are now either randomly selected for you or you can pass in an
    hex/rgb value. Kinda want you to pass in a hex value, much easier to
    deal with.
    
    Something seems to be wrong with how i'm determining the intensity in
    the HSL value. Sometimes, it comes back all black. hurp

--- js/rtsHeatMap.js
+++ js/rtsHeatMap.js
@@ -7,7 +7,7 @@
 		mapHeight : null, // If null, i'll use the parent DOM height if possible (otherwise, fallback 200px).
 		mapWidth : null, // If null, i'll use the parent DOM width
 
-		mapSegmentColors : {}, // You can provide either a range of colors (between three values), or if 4 our more, i'll do some crazy math. 2 or less will result in my defaults, not yours. Not sure i like this...
+		mapSegmentColor : "#89bc62", // You must provide me and RGB value (rgb(255,255,255)) and i'll use this (in HSL format) only OR the word random, then i'll pick one for you
 		mapSegmentRadius : 0,
 
 		mapColumnSegmentDimensions : {width:15,height:15}, // Dimensions, in pixels. (10px height, 10px width)
@@ -43,20 +43,78 @@
 
 		function log(msg) { console.log(msg); }
 
-		function tempTorgb(data, max) {
-			var temp = data / max;
-			var red = blue = green = 0;
-			if(temp >= 0 && temp < .5) { //green stays at 100% and red raises to 100%
-				green = 1;
-				red = 2 * temp;
+		// http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb (modified for this plugin)
+		function hexToRgb(hex) {
+		    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		    return result ? {r:parseInt(result[1], 16),g:parseInt(result[2], 16),b:parseInt(result[3], 16)} : null;
+		}
+
+		// http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+		/**
+		 * Converts an HSL color value to RGB. Conversion formula
+		 * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+		 * Assumes h, s, and l are contained in the set [0, 1] and
+		 * returns r, g, and b in the set [0, 255].
+		 *
+		 * @param   Number  h       The hue
+		 * @param   Number  s       The saturation
+		 * @param   Number  l       The lightness
+		 * @return  Array           The RGB representation
+		 */
+		function hslToRgb(h, s, l){
+			var r, g, b;
+
+			if(s == 0){
+				r = g = b = l; // achromatic
+			}else{
+				function hue2rgb(p, q, t){
+					if(t < 0) t += 1;
+					if(t > 1) t -= 1;
+					if(t < 1/6) return p + (q - p) * 6 * t;
+					if(t < 1/2) return q;
+					if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+					return p;
+				}
+
+				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+				var p = 2 * l - q;
+				r = hue2rgb(p, q, h + 1/3);
+				g = hue2rgb(p, q, h);
+				b = hue2rgb(p, q, h - 1/3);
 			}
-			if (temp >= .5 && temp <= 1) { //red stays at 100% and green decays
-				red = 1;
-				green = 2 * (1- temp); //1 - 2 * ($temp - 0.5);
+
+			return {r:r * 255, g:g * 255, b:b * 255};
+		}
+		/**
+		 * Converts an RGB color value to HSL. Conversion formula
+		 * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+		 * Assumes r, g, and b are contained in the set [0, 255] and
+		 * returns h, s, and l in the set [0, 1].
+		 *
+		 * @param   Number  r       The red color value
+		 * @param   Number  g       The green color value
+		 * @param   Number  b       The blue color value
+		 * @return  Array           The HSL representation
+		 */
+		function rgbToHsl(r, g, b){
+			r /= 255, g /= 255, b /= 255;
+			var max = Math.max(r, g, b), min = Math.min(r, g, b);
+			var h, s, l = (max + min) / 2;
+
+			if(max == min){
+				h = s = 0; // achromatic
+			}else{
+				var d = max - min;
+				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+				switch(max){
+					case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+					case g: h = (b - r) / d + 2; break;
+					case b: h = (r - g) / d + 4; break;
+				}
+				h /= 6;
 			}
-			red = Math.round(red * 255);
-			green = Math.round(green * 255);
-			return 'rgba({0},{1},{2},{3})'.format(red,green,blue,temp/.2);
+
+			return {h:h, s:s, l:l};
 		}
 
 		// Custom Helpers!
@@ -84,35 +142,14 @@
 			// http://learn.jquery.com/plugins/advanced-plugin-concepts/
 			var opts = $.extend({},settings,$this.data());
 
+			// My random color do-hicky
+			if(opts.mapSegmentColor == 'random')
+				opts.mapSegmentColor = '#'+Math.floor(Math.random()*16777215).toString(16); // http://www.paulirish.com/2009/random-hex-color-code-snippets/
+
 			// Create the canvas element for each object.
 			var canvObj = createCanvasObject($this, opts); // now can use $(canvObj) to do canvas methods
 
 			$(this).append(canvObj); // write the new canvas to the parent.
-
-			// Do work (simple, i'll make it better later)
-			/*
-			$i = 0;
-			foreach($data as $k => $v) {
-				$created = date_create($k);
-				$dayOfYear = $created->format('z'); // Starts at 0 ends at 364
-				$dayOfWeek = $created->format('N'); // Starts at 1 (Monday), ends at 7 (Sunday)
-				$class = tempTorgb($v,$scaleBase);
-				if($v == 0 && $created->format('U') - time() > 0) // If a future date, we don't have data, so show white instead
-					$class = 'white';
-
-				if($i == 0) echo sprintf('<div class="col">'); // If first day of a valid week
-
-				$divClass = ($created->format('Y') != $thisYear) ? 'square past' : 'square'; // Don't want to show a old square value!
-				$class = ($created->format('Y') != $thisYear) ? 'none' : $class;
-
-				echo sprintf('<div title="%s: %s" class="%s" style="background-color:%s"></div>',$created->format('l m/d/Y'), $v,$divClass, $class);
-
-				if($i++ == 6 || ($created->format('Y') == $thisYear && $dayOfYear == 365)) {
-					$i = 0; // reset so we can start a new week
-					echo sprintf('</div>');
-				}
-			}
-			*/
 
 			// Get Max value of the object array
 			var highest = Number.NEGATIVE_INFINITY;
@@ -129,7 +166,12 @@
 				var val = opts.mapData[key];
 
 				// Determine Fill Style
-				var fillStyle = tempTorgb(val,highest);
+				fillStyle = hexToRgb(opts.mapSegmentColor); // This will either be null (it wasn't hex) or an string rgb value
+				if(!fillStyle) fillStyle = opts.mapSegmentColor; // returns {r:0,g:0,b:0} object
+				var hsl = rgbToHsl(fillStyle.r,fillStyle.g,fillStyle.b); // Canvas does accept hsl values. the L
+				hsl.l = val / highest; // Need to determine the "L" instensity value.
+				var rgb = hslToRgb(hsl.h,hsl.s,hsl.l);
+				fillStyle = 'rgb({0},{1},{2})'.format(Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b));
 
 				// Determine X position (column)
 				// if i % mapColumnMaxSegmentCount = 0, then we're at the start of a new column, x should be i + opts.mapColumnSegmentDimensions.width
@@ -151,7 +193,7 @@
 					x: x,  // Starting from 0, then until max segmentCount is reached, then its value + segment width (in addition to borders)
 					y: y, // Starting from 0, then value + segmentHeight, then until max segmentCount is reached, the its back to 0
 					strokeStyle: "rgba(37,51,148,0.1)",
-  					strokeWidth: 1,
+					strokeWidth: 1,
 					width: opts.mapColumnSegmentDimensions.width,
 					height: opts.mapColumnSegmentDimensions.height,
 					fromCenter: false, // MUST SET THIS FOR EASIER POSITIONING
