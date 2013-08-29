c4f9a03e97fd4b63e3055d805f2455c00558ba82
Richard Shaw <richard.shaw@theactigraph.com>
2013-08-28 21:31:41 -0500

    Completely changed up how the plugin works.
    The plugin now support public methods. You must called
    domObj.data('rtsHeatMap'); to get access to the public methods. This is
    to help clean up the jQuery namespace.
    
    New methods will follow soon to allow manipulation of the map after
    creation. So far, you can random change the colors of the segments
    without having to rebuild the entire map!

--- js/rtsHeatMap.js
+++ js/rtsHeatMap.js
@@ -2,6 +2,7 @@
 (function($) {
 
 	$.fn.rtsHeatMapDefaults =  {
+
 		mapBackgroundcolor : "white", // background color of map, the canvas' element color
 
 		mapHeight : null, // If null, i'll use the parent DOM height if possible (otherwise, fallback 200px).
@@ -20,18 +21,98 @@
 		mapComplete : null // Once all maps have been renedered, i'll call this
 	};
 
-	$.fn.rtsHeatMap = function(options){
+	// http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb (modified for this plugin)
+	function hexToRgb(hex) {
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	    return result ? {r:parseInt(result[1], 16),g:parseInt(result[2], 16),b:parseInt(result[3], 16)} : null;
+	}
+	// http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+	function hslToRgb(h, s, l){
+		var r, g, b;
 
+		if(s == 0){
+			r = g = b = l; // achromatic
+		}else{
+			function hue2rgb(p, q, t){
+				if(t < 0) t += 1;
+				if(t > 1) t -= 1;
+				if(t < 1/6) return p + (q - p) * 6 * t;
+				if(t < 1/2) return q;
+				if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+				return p;
+			}
+
+			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			var p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1/3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3);
+		}
+
+		return {r:r * 255, g:g * 255, b:b * 255};
+	}
+	function rgbToHsl(r, g, b){
+		r /= 255, g /= 255, b /= 255;
+		var max = Math.max(r, g, b), min = Math.min(r, g, b);
+		var h, s, l = (max + min) / 2;
+
+		if(max == min){
+			h = s = 0; // achromatic
+		}else{
+			var d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch(max){
+				case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+				case g: h = (b - r) / d + 2; break;
+				case b: h = (r - g) / d + 4; break;
+			}
+			h /= 6;
+		}
+
+		return {h:h, s:s, l:l};
+	}
+	function log(msg)  { console.log(msg); }
+	Object.size = function(obj) { var size = 0, key; for (key in obj) { if (obj.hasOwnProperty(key)) size++; } return size; };
+	String.prototype.format = function() { var args = arguments; return this.replace(/\{(\d+)\}/g, function(match, number) { return typeof args[number] != 'undefined' ? args[number] : match; }); };
+	// Array.prototype.max = function() {  return Math.max.apply(null, this); };
+	// Array.prototype.min = function() {  return Math.min.apply(null, this); };
+
+	// Main Entry point for plugin
+	var RTSHeatMap = function(element, options) {
+		var $this = element;
+		var obj = this;
+
+		// I support the Data option!
+		// http://learn.jquery.com/plugins/advanced-plugin-concepts/
 		var settings = $.extend({},$.fn.rtsHeatMapDefaults,options);
+		var opts = $.extend({},settings,$this.data());
+
+		// Public methods
+		this.set = function(name,value) { if(opts[name]) opts[name] = value; return this; };
+		this.get = function(name) { return opts[name] || null; };
+
+		// More useful methods
+		this.setMapSegmentColor = function(color) {
+			opts.mapSegmentColor = color;
+			// Get the layers (segments) of this canvas object and change their colors ><
+			var domObj = $this.find('canvas');
+			var layers = domObj.getLayers();
+			for(var key in layers) {
+				var layer = layers[key];
+				domObj.setLayer(layer,{
+					fillStyle : getFillStyle(layer.data.dataValue) // hurp...
+				})
+			}
+			domObj.drawLayers(); // once all layers have their colors, draw them
+		}
 
 		// Private
-		function createCanvasObject(domObj, opts)
-		{
+		var createCanvasObject = function(domObj) {
 			// objID should be the parent's domObj. I'll use id to unique this guy, or a name
 			var objName = "rtsMap";
 			if(domObj.attr('id') != "") objName = domObj.attr('id')+"rtsHeatMap";
 
-			return $('<canvas/>')
+			var cObj = $('<canvas/>')
 				.attr({
 					'id' :objName,
 					'height' : (opts.mapHeight) ? opts.mapHeight : $this.height(),
@@ -41,183 +122,90 @@
 					'background-color' : opts.mapBackgroundcolor
 				})
 				.text('Your browser does not support canvas. Please upgrade your browser');
+
+			domObj.append(cObj); // write the new canvas to the parent.
+			return cObj;
 		}
 
-		function log(msg) { console.log(msg); }
+		var getFillStyle = function(val) {
+			// Determine Fill Style
+			fillStyle = hexToRgb(opts.mapSegmentColor); // This will either be null (it wasn't hex) or an string rgb value
+			if(!fillStyle) fillStyle = opts.mapSegmentColor; // returns {r:0,g:0,b:0} object
 
-		// http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb (modified for this plugin)
-		function hexToRgb(hex) {
-		    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-		    return result ? {r:parseInt(result[1], 16),g:parseInt(result[2], 16),b:parseInt(result[3], 16)} : null;
+			var hsl = rgbToHsl(fillStyle.r,fillStyle.g,fillStyle.b); // Canvas does accept hsl values. the L
+			hsl.l = Math.abs( opts.mapSegmentMinLightness + (val * ratio) - 1 ); // Flips dark/light so its lower the number, lighter the color.
+
+			var rgb = hslToRgb(hsl.h,hsl.s,hsl.l);
+			fillStyle = 'rgb({0},{1},{2})'.format(Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b));
+
+			return fillStyle;
 		}
 
-		// http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
-		/**
-		 * Converts an HSL color value to RGB. Conversion formula
-		 * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-		 * Assumes h, s, and l are contained in the set [0, 1] and
-		 * returns r, g, and b in the set [0, 255].
-		 *
-		 * @param   Number  h       The hue
-		 * @param   Number  s       The saturation
-		 * @param   Number  l       The lightness
-		 * @return  Array           The RGB representation
-		 */
-		function hslToRgb(h, s, l){
-			var r, g, b;
+		// My random color do-hicky
+		// http://www.paulirish.com/2009/random-hex-color-code-snippets/
+		if(opts.mapSegmentColor == 'random')
+			opts.mapSegmentColor = '#'+('000000' + Math.floor(Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
+		if(opts.mapBackgroundcolor == 'random')
+			opts.mapBackgroundcolor = '#'+('000000' + Math.floor(Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
 
-			if(s == 0){
-				r = g = b = l; // achromatic
-			}else{
-				function hue2rgb(p, q, t){
-					if(t < 0) t += 1;
-					if(t > 1) t -= 1;
-					if(t < 1/6) return p + (q - p) * 6 * t;
-					if(t < 1/2) return q;
-					if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-					return p;
-				}
+		// Create the canvas element for each object.
+		var canvObj = createCanvasObject($this); // now can use $(canvObj) to do canvas methods
 
-				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-				var p = 2 * l - q;
-				r = hue2rgb(p, q, h + 1/3);
-				g = hue2rgb(p, q, h);
-				b = hue2rgb(p, q, h - 1/3);
+		// Get Max/Min/Ratio value of the object array
+		var lowest = Number.POSITIVE_INFINITY;
+		var highest = Number.NEGATIVE_INFINITY;
+		var tmp;
+		for (var key in opts.mapData) {
+			tmp = opts.mapData[key];
+			if(tmp < lowest) lowest = tmp;
+			if(tmp > highest) highest = tmp;
+		}
+		var ratio = (1 - opts.mapSegmentMinLightness - (1-opts.mapSegmentMaxLightness))/(highest - lowest);
+
+		var i = 0;
+		var columnCount = 0;
+		for(var key in opts.mapData) {
+
+			// Determine X position (column)
+			// if i % mapColumnMaxSegmentCount = 0, then we're at the start of a new column, x should be i + opts.mapColumnSegmentDimensions.width
+			var x = columnCount * opts.mapColumnSegmentDimensions.width; // First value
+			if(i !=0 && i % opts.mapColumnMaxSegmentCount == 0) // ... i'm at the start of a new column
+				x = (++columnCount) * opts.mapColumnSegmentDimensions.width; // Move x over, increase ColumnCount for next trip
+
+			// Determine Y position (row)
+			var y = (i % opts.mapColumnMaxSegmentCount == 0) ? 0 : y;
+			if(i % opts.mapColumnMaxSegmentCount > 0) { // I'm withing a column, keep adding height
+				y += opts.mapColumnSegmentDimensions.height;
 			}
 
-			return {r:r * 255, g:g * 255, b:b * 255};
-		}
-		/**
-		 * Converts an RGB color value to HSL. Conversion formula
-		 * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-		 * Assumes r, g, and b are contained in the set [0, 255] and
-		 * returns h, s, and l in the set [0, 1].
-		 *
-		 * @param   Number  r       The red color value
-		 * @param   Number  g       The green color value
-		 * @param   Number  b       The blue color value
-		 * @return  Array           The HSL representation
-		 */
-		function rgbToHsl(r, g, b){
-			r /= 255, g /= 255, b /= 255;
-			var max = Math.max(r, g, b), min = Math.min(r, g, b);
-			var h, s, l = (max + min) / 2;
-
-			if(max == min){
-				h = s = 0; // achromatic
-			}else{
-				var d = max - min;
-				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-				switch(max){
-					case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-					case g: h = (b - r) / d + 2; break;
-					case b: h = (r - g) / d + 4; break;
+			// Create square
+			$(canvObj).drawRect({
+				layer: true,
+				fillStyle: getFillStyle(opts.mapData[key]), // This is the hard part (setting the color)
+				x: x,  // Starting from 0, then until max segmentCount is reached, then its value + segment width (in addition to borders)
+				y: y, // Starting from 0, then value + segmentHeight, then until max segmentCount is reached, the its back to 0
+				strokeStyle: "rgba(37,51,148,0.1)",
+				strokeWidth: 1,
+				width: opts.mapColumnSegmentDimensions.width,
+				height: opts.mapColumnSegmentDimensions.height,
+				fromCenter: false, // MUST SET THIS FOR EASIER POSITIONING
+				cornerRadius: opts.mapSegmentRadius,
+				data: {
+					dataValue : opts.mapData[key] // Use this for future repaints and other options (so we're not constantly looping over the data array)
 				}
-				h /= 6;
-			}
+			});
 
-			return {h:h, s:s, l:l};
-		}
-
-		// Custom Helpers!
-		Object.size = function(obj) {
-			var size = 0, key;
-			for (key in obj) if (obj.hasOwnProperty(key)) size++;
-			return size;
+			i++; // required
 		};
-
-		// example : "{0} : {1}".format("string", "string")
-		String.prototype.format = function() {
-			var args = arguments;
-			return this.replace(/\{(\d+)\}/g, function(match, number) { return typeof args[number] != 'undefined' ? args[number] : match; });
-		};
-		Array.prototype.max = function() {  return Math.max.apply(null, this); };
-		Array.prototype.min = function() {  return Math.min.apply(null, this); };
+	}
 
 
-
-		// Main Methods
+	$.fn.rtsHeatMap = function(options) {
 		return this.each(function(){
 			$this = $(this); // hurp
-
-			log('====================================================================================================================');
-
-			// I support the Data option!
-			// http://learn.jquery.com/plugins/advanced-plugin-concepts/
-			var opts = $.extend({},settings,$this.data());
-
-			// My random color do-hicky
-			// http://www.paulirish.com/2009/random-hex-color-code-snippets/
-			if(opts.mapSegmentColor == 'random')
-				opts.mapSegmentColor = '#'+('000000' + Math.floor(Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
-			if(opts.mapBackgroundcolor == 'random')
-				opts.mapBackgroundcolor = '#'+('000000' + Math.floor(Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
-
-			// Create the canvas element for each object.
-			var canvObj = createCanvasObject($this, opts); // now can use $(canvObj) to do canvas methods
-
-			$(this).append(canvObj); // write the new canvas to the parent.
-
-			// Get Max/Min/Ratio value of the object array
-			var lowest = Number.POSITIVE_INFINITY;
-			var highest = Number.NEGATIVE_INFINITY;
-			var tmp;
-			for (var key in opts.mapData) {
-				tmp = opts.mapData[key];
-				if(tmp < lowest) lowest = tmp;
-				if(tmp > highest) highest = tmp;
-			}
-			var ratio = (1 - opts.mapSegmentMinLightness - (1-opts.mapSegmentMaxLightness))/(highest - lowest);
-
-
-			var i = 0;
-			var columnCount = 0;
-			for(var key in opts.mapData) {
-
-				var val = opts.mapData[key];
-
-				// Determine Fill Style
-				fillStyle = hexToRgb(opts.mapSegmentColor); // This will either be null (it wasn't hex) or an string rgb value
-				if(!fillStyle) fillStyle = opts.mapSegmentColor; // returns {r:0,g:0,b:0} object
-
-				var hsl = rgbToHsl(fillStyle.r,fillStyle.g,fillStyle.b); // Canvas does accept hsl values. the L
-				hsl.l = Math.abs( opts.mapSegmentMinLightness + (val * ratio) - 1 ); // Flips dark/light so its lower the number, lighter the color.
-
-				var rgb = hslToRgb(hsl.h,hsl.s,hsl.l);
-				fillStyle = 'rgb({0},{1},{2})'.format(Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b));
-
-				// Determine X position (column)
-				// if i % mapColumnMaxSegmentCount = 0, then we're at the start of a new column, x should be i + opts.mapColumnSegmentDimensions.width
-				var x = columnCount * opts.mapColumnSegmentDimensions.width; // First value
-				if(i !=0 && i % opts.mapColumnMaxSegmentCount == 0) // ... i'm at the start of a new column
-					x = (++columnCount) * opts.mapColumnSegmentDimensions.width; // Move x over, increase ColumnCount for next trip
-
-				// Determine Y position (row)
-				var y = (i % opts.mapColumnMaxSegmentCount == 0) ? 0 : y;
-				if(i % opts.mapColumnMaxSegmentCount > 0) { // I'm withing a column, keep adding height
-					y += opts.mapColumnSegmentDimensions.height;
-				}
-
-				// Create square
-				$(canvObj).drawRect({
-					fillStyle: fillStyle, // This is going to be the hard part (setting the color)
-					x: x,  // Starting from 0, then until max segmentCount is reached, then its value + segment width (in addition to borders)
-					y: y, // Starting from 0, then value + segmentHeight, then until max segmentCount is reached, the its back to 0
-					strokeStyle: "rgba(37,51,148,0.1)",
-					strokeWidth: 1,
-					width: opts.mapColumnSegmentDimensions.width,
-					height: opts.mapColumnSegmentDimensions.height,
-					fromCenter: false, // MUST SET THIS FOR EASIER POSITIONING
-					cornerRadius: opts.mapSegmentRadius
-				});
-
-				i++; // required
-			};
-
-
-
+			if($this.data('rtsHeatMap')) return; // bailout if we already have the map init'd
+			$this.data('rtsHeatMap',new RTSHeatMap($this, options));
 		});
-
 	};
 
 }(jQuery));
